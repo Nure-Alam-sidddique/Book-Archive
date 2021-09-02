aa832b047d66a527e4efde418984f52fb49fa3f0
Nure-Alam-sidddique <74979607+Nure-Alam-sidddique@users.noreply.github.com>
2021-09-02 18:20:37 +0600

    comment writing

--- book-archive-Nure-Alam-sidddique/JS/app.js
+++ book-archive-Nure-Alam-sidddique/JS/app.js
@@ -5,20 +5,24 @@ const bookCard = document.querySelector('#book-card');
 const errorMessage = style => {
     document.querySelector('#error-message').style.display = style;
 }
+//  display block and None function
 const searchDisplaNone = style => {
     document.getElementById('search-result').style.display = style;
 }
+//  search Total result count function
 const searchResult = (result) => {
     const resultStyle = document.querySelector("#search-result");
   resultStyle.innerHTML=`<h3 class="text-success left-border ps-1">Total Search Result : <span id="result">${result}</span></h3>`;
 }
+//  spinner Loading function
 const toggleSpinner = style => {
   document.querySelector('#spinner-loading').style.display = style;
 }
 //  Click button for Load data form API
 searchBtn.addEventListener('click', event => {
     // console.log("I love Allah");
-    const searchText = searchField.value;
+  const searchText = searchField.value;
+  //  Erro Handling part
     if (searchText === "") {
         errorMessage('block');
         bookCard.textContent = '';
@@ -30,20 +34,24 @@ searchBtn.addEventListener('click', event => {
         const url = ` https://openlibrary.org/search.json?q=${searchText}`;
       searchField.value = "";
       toggleSpinner('block');
+      // load data
         fetch(url)
           .then((response) => response.json())
             .then((data) => bookDisplay(data));
     }
 })
+//  calling after loading and  Display book function
 const bookDisplay = bookObj => {
-    console.log(bookObj);
+    // console.log(bookObj);
     bookCard.textContent = '';
     const books = bookObj.docs;
     // console.log(books)
+  //  Loop Array
     books.slice(0,12).forEach(book => {
         // console.log(book);
         const div = document.createElement('div');
-        div.classList.add('col');
+      div.classList.add('col');
+      //  Card Create to show display
         div.innerHTML = `
                <div class="card h-100 " id="card">
               <img src=" https://covers.openlibrary.org/b/id/${
@@ -75,6 +83,7 @@ const bookDisplay = bookObj => {
         `;
         bookCard.appendChild(div);
     });
+  //  After card loading this function call to show total count book Number from the search and do display block and toggole spinner display none
     searchResult(bookObj.num_found);
   searchDisplaNone('block');
   toggleSpinner('none');
